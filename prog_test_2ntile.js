288aa95f5dce43c770dca37eabd7ff0aa377cbff
Tofulim <vail131@naver.com>
2020-11-10 16:46:16 +0900

    baekjoon

--- prog_test_2ntile.js
+++
@@ -1,9 +0,0 @@
-function solution(n) {
-    var dp=new Array(n+1);
-    dp[1]=1; dp[2]=2;
-    if(n==1||n==2) return dp[n];
-    for(var i=3;i<=n;i++){
-        dp[i]=(dp[i-1]+dp[i-2])%1000000007;
-    }
-    return dp[n];
-}
